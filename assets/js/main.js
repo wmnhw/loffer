8898da3984473e078ddf668cacf9af874bae2ea8
wmnhw <69662838+wmnhw@users.noreply.github.com>
2021-01-07 02:27:06 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,28 @@ window.onscroll = function() {
   } else {
     top.style.display = "none";
   }
+  
+  // 高亮目录
+  if(document.querySelector('#markdown-toc')) {  // 是否生成了目录
+    if (document.querySelector('.tocactive')) {  // 首先清除原有高亮
+        document.querySelector('.tocactive').classList.remove('tocactive');
+    }
+    
+    var subTitles = document.querySelectorAll('.post h2, .post h3'); // 所有标题
+    for (var i = 0; i < subTitles.length; i++) {
+      if (subTitles[i].offsetTop + 2 >= t || i === subTitles.length - 1) {         // 可视窗口及下方的第一个标题
+        if(subTitles[i].offsetTop - 2 >= t) {  // 标题上方还有一些内容
+          if( i > 0) {  // 渲染上一个标题
+            document.querySelector('#markdown-toc-' + subTitles[i-1].textContent).classList.add('tocactive');
+          }
+        } else {  // 渲染当前标题
+          document.querySelector('#markdown-toc-' + subTitles[i].textContent).classList.add('tocactive');
+        }
+        break;
+      } 
+    }
+  }
+};
 
 // 回到顶部滚动函数
 function toScroll() {
@@ -23,3 +45,19 @@ function toScroll() {
     };
   }, 10)  // 每 10 毫秒调用一次，这样绝对顺滑
 };
+
+// 导航栏中的搜索框回车触发弹出框
+document.querySelector('#search-input').addEventListener('keyup', function(e) {
+  if(e.keyCode == 13){
+    if(this.value.trim() != '') {  // 删除头尾空格
+      location.href = '{{ site.baseurl }}/search.html?keyword=' + this.value;
+    }
+  }
+});
+
+var imgs = document.getElementsByClassName('bigimg');
+for(var i = 0; i < imgs.length; i++) {
+  imgs[i].addEventListener('click', function(){
+    window.open(this.src,'_blank');
+  })
+}
